c6fe7ed620e0748906f451d172d2ba56ded91793
KhanhND <duckhanh.0811@gmail.com>
2024-04-21 23:37:15 +0700

    fix style and add loading

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,66 +1,69 @@
 'use client';
 import { Container } from "@/component/container";
-import { Button, Checkbox, Form, FormProps, Input } from "antd"
-import styled from "styled-components";
+import { Button, Checkbox, Form, FormProps, Input } from "antd";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 type FieldType = {
     username: string;
     password: string;
     remember: boolean
 }
-// const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
-//     console.log('Success:', values);
-// };
-
 const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
     console.log('Failed:', errorInfo);
 };
-
 const Login = () => {
-    const onFinish = () => {
-
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const router = useRouter()
+    const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+        setIsLoading(true);
+        console.log('Success:', values);
+        router.push('/');
     }
+
     return (
         <Container>
-            <Form
-                name="basic"
-                labelCol={{ span: 8 }}
-                wrapperCol={{ span: 16 }}
-                style={{ maxWidth: 600 }}
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete="off"
-            >
-                <Form.Item<FieldType>
-                    label="Username"
-                    name="username"
-                    rules={[{ required: true, message: 'Please input your username!' }]}
+            <div className="wrap-container">
+                <Form
+                    name="basic"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    style={{ maxWidth: 600 }}
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
                 >
-                    <Input />
-                </Form.Item>
+                    <Form.Item<FieldType>
+                        label="Username"
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
 
-                <Form.Item<FieldType>
-                    label="Password"
-                    name="password"
-                    rules={[{ required: true, message: 'Please input your password!' }]}
-                >
-                    <Input.Password />
-                </Form.Item>
+                    <Form.Item<FieldType>
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
 
-                <Form.Item<FieldType>
-                    name="remember"
-                    valuePropName="checked"
-                    wrapperCol={{ offset: 8, span: 16 }}
-                >
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item>
+                    <Form.Item<FieldType>
+                        name="remember"
+                        valuePropName="checked"
+                        wrapperCol={{ offset: 8, span: 16 }}
+                    >
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
 
-                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                    <Button type="primary" htmlType="submit">
-                        Submit
-                    </Button>
-                </Form.Item>
-            </Form>
+                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                        <Button type="primary" htmlType="submit" loading={isLoading} >
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
         </Container>
     )
 }
--- src/component/container.ts
+++ src/component/container.ts
@@ -1,8 +1,18 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled('div')`
     display: flex;
     justify-content: center;
     min-height: 100vh;
     align-items: center;
+    .wrap-container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: 1px solid #cec1c1ee;
+        border-radius: 10px;
+        padding: 20px;
+        background: white
+    }
+
 `
