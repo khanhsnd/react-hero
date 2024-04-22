97c538e8aa5978d7049923fd07e1fcf06adeb8b6
KhanhND <duckhanh.0811@gmail.com>
2024-04-22 22:59:10 +0700

    add main layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import StyledComponentsRegistry from "@/lib/registry";
+import MainLayout from "./layout/MainLayout";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,12 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <StyledComponentsRegistry>
-          <AntdRegistry>{children}</AntdRegistry>
+          <AntdRegistry>
+            <MainLayout>
+              {children}
+            </MainLayout>
+
+          </AntdRegistry>
         </StyledComponentsRegistry>
       </body>
     </html>
--- src/type/GlobalType.tsx
+++ src/type/GlobalType.tsx
@@ -1,3 +1,4 @@
 export interface Props {
     children: React.ReactNode;
 }
+export type PromiseType<T> = T | Promise<T>;
